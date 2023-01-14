f54228752439c6899a249d3cd347ad1669431200
ippsav <mehdiboujid@gmail.com>
2023-01-14 18:00:57 +0000

    fix formatting

--- src/cli.rs
+++ src/cli.rs
@@ -1,25 +1,22 @@
-use clap::{Parser, Subcommand, Args};
-
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
 pub struct Cli {
-   #[command(subcommand)]
-   pub commands: Commands
+    #[command(subcommand)]
+    pub commands: Commands,
 }
-
-
 
 #[derive(Subcommand)]
 pub enum Commands {
-  Set(Set)
+    Set(Set),
 }
 
 #[derive(Args, Debug)]
-pub struct Set{
-  #[arg(short, long, help="Set a secret in firebase, ex: KEY=VALUE")]
-  pub secret: Option<String>,
-  #[arg(short, long, help="Path to a file as a source of secrets")]
-  pub path: Option<String>
+pub struct Set {
+    #[arg(short, long, help = "Set a secret in firebase, ex: KEY=VALUE")]
+    pub secret: Option<String>,
+    #[arg(short, long, help = "Path to a file as a source of secrets")]
+    pub path: Option<String>,
 }
